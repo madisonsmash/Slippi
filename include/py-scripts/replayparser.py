59acba71f8606ace2eaf9d7e6c3ddb04d610080c
Andrew Bennett <awbennett@wisc.edu>
2019-07-21 17:28:45 -0500

    Add images, begin work on python script

--- include/py-scripts/replayparser.py
+++ include/py-scripts/replayparser.py
@@ -3,5 +3,11 @@ from os import listdir
 from os.path import isfile, join
 from slippi import Game
 
-replays = [f for f in listdir("replays") if isfile(join("replays", f))]
+replays = [f for f in listdir("../../replays") if isfile(join("../../replays", f))]
 
+#dictionary to serialize to json
+map = {}
+
+game = Game('../../replays//'+replays[0])
+
+print(game)
